46ff89edd7c1848699fa5ba4cc2d0b175e0ffb94
Joseph Collard <jcollard@users.noreply.github.com>
2021-10-02 15:22:40 -0700

    chore: Add stylecop to individual project.

---
+++ tests/TestDecodeCardPair.cs
@@ -0,0 +1,32 @@
+namespace BytesAndData
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests <see cref="IFiveTwoOneOneDecoder"/> DecodeCardPair method.
+    /// </summary>
+    public class TestDecodeCardPair
+    {
+        private IFiveTwoOneOneDecoder decoder;
+
+        /// <summary>
+        /// Initializes the IFiveTwoOneOneDecoder by using <see
+        /// cref="Config.GetDecoder"/>.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.decoder = new Config().GetDecoder();
+        }
+
+        /// <summary>
+        /// Tests decoding a valid Card Pair.
+        /// </summary>
+        [Test]
+        public void DecodeCardPairValid()
+        {
+            Assert.AreEqual(this.decoder.DecodeCardPair(0x00, 0x34), "Face Down Orange Four");
+            Assert.AreEqual(this.decoder.DecodeCardPair(0x01, 0x22), "Face Up Blue Two");
+        }
+    }
+}
