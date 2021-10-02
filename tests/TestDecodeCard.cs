26f6b490ee1f8ab56b5544112a524733d29c029f
Joseph Collard <jcollard@users.noreply.github.com>
2021-10-02 12:24:41 -0700

    feat: Add test for DecodeCard and DecodeHand

---
+++ tests/TestDecodeCard.cs
@@ -0,0 +1,92 @@
+namespace BytesAndData
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests <see cref="IFiveTwoOneOneDecoder"/> DecodeCard method.
+    /// </summary>
+    public class TestDecodeCard
+    {
+        private IFiveTwoOneOneDecoder decoder;
+
+        /// <summary>
+        /// Initializes the IFiveTwoOneOneDecoder by using <see
+        /// cref="Config.GetDecoder"/>.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.decoder = new Config().GetDecoder();
+        }
+
+        /// <summary>
+        /// Tests decoding Yellow Cards.
+        /// </summary>
+        [Test]
+        public void DecodeCardYellowCards()
+        {
+            Assert.AreEqual(this.decoder.DecodeCard(0x01), "Yellow Rooster");
+            Assert.AreEqual(this.decoder.DecodeCard(0x02), "Yellow Two");
+            Assert.AreEqual(this.decoder.DecodeCard(0x03), "Yellow Three");
+            Assert.AreEqual(this.decoder.DecodeCard(0x04), "Yellow Four");
+            Assert.AreEqual(this.decoder.DecodeCard(0x05), "Yellow Five");
+            Assert.AreEqual(this.decoder.DecodeCard(0x06), "Yellow Six");
+        }
+
+        /// <summary>
+        /// Tests decoding Green Cards.
+        /// </summary>
+        [Test]
+        public void DecodeCardGreenCards()
+        {
+            Assert.AreEqual(this.decoder.DecodeCard(0x11), "Green Rooster");
+            Assert.AreEqual(this.decoder.DecodeCard(0x12), "Green Two");
+            Assert.AreEqual(this.decoder.DecodeCard(0x13), "Green Three");
+            Assert.AreEqual(this.decoder.DecodeCard(0x14), "Green Four");
+            Assert.AreEqual(this.decoder.DecodeCard(0x15), "Green Five");
+            Assert.AreEqual(this.decoder.DecodeCard(0x16), "Green Six");
+        }
+
+        /// <summary>
+        /// Tests decoding Green Cards.
+        /// </summary>
+        [Test]
+        public void DecodeCardBlueCards()
+        {
+            Assert.AreEqual(this.decoder.DecodeCard(0x21), "Blue Rooster");
+            Assert.AreEqual(this.decoder.DecodeCard(0x22), "Blue Two");
+            Assert.AreEqual(this.decoder.DecodeCard(0x23), "Blue Three");
+            Assert.AreEqual(this.decoder.DecodeCard(0x24), "Blue Four");
+            Assert.AreEqual(this.decoder.DecodeCard(0x25), "Blue Five");
+            Assert.AreEqual(this.decoder.DecodeCard(0x26), "Blue Six");
+        }
+
+        /// <summary>
+        /// Tests decoding Orange Cards.
+        /// </summary>
+        [Test]
+        public void DecodeCardOrangeCards()
+        {
+            Assert.AreEqual(this.decoder.DecodeCard(0x31), "Orange Rooster");
+            Assert.AreEqual(this.decoder.DecodeCard(0x32), "Orange Two");
+            Assert.AreEqual(this.decoder.DecodeCard(0x33), "Orange Three");
+            Assert.AreEqual(this.decoder.DecodeCard(0x34), "Orange Four");
+            Assert.AreEqual(this.decoder.DecodeCard(0x35), "Orange Five");
+            Assert.AreEqual(this.decoder.DecodeCard(0x36), "Orange Six");
+        }
+
+        /// <summary>
+        /// Tests decoding Black Cards.
+        /// </summary>
+        [Test]
+        public void DecodeCardBlackCards()
+        {
+            Assert.AreEqual(this.decoder.DecodeCard(0x41), "Black Rooster");
+            Assert.AreEqual(this.decoder.DecodeCard(0x42), "Black Two");
+            Assert.AreEqual(this.decoder.DecodeCard(0x43), "Black Three");
+            Assert.AreEqual(this.decoder.DecodeCard(0x44), "Black Four");
+            Assert.AreEqual(this.decoder.DecodeCard(0x45), "Black Five");
+            Assert.AreEqual(this.decoder.DecodeCard(0x46), "Black Six");
+        }
+    }
+}
---
+++ tests/TestDecodeHand.cs
@@ -0,0 +1,42 @@
+namespace BytesAndData
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests <see cref="IFiveTwoOneOneDecoder"/> DecodeHand method.
+    /// </summary>
+    public class TestDecodeHand
+    {
+        private IFiveTwoOneOneDecoder decoder;
+
+        /// <summary>
+        /// Initializes the IFiveTwoOneOneDecoder by using <see
+        /// cref="Config.GetDecoder"/>.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.decoder = new Config().GetDecoder();
+        }
+
+        /// <summary>
+        /// Tests decoding Yellow Cards.
+        /// </summary>
+        [Test]
+        public void DecodeHand0()
+        {
+            byte[] hand = { 0x00, 0x00, 0x00, 0x01, 0x25, 0x34, 0x31, 0x43 };
+            Assert.AreEqual(this.decoder.DecodeHand(hand), "Yellow Rooster, Blue Five, Orange Four, Orange Rooster, Black Three");
+        }
+
+        /// <summary>
+        /// Tests decoding Yellow Cards.
+        /// </summary>
+        [Test]
+        public void DecodeHand1()
+        {
+            byte[] hand = { 0x00, 0x00, 0x00, 0x22, 0x33, 0x04, 0x12, 0x06 };
+            Assert.AreEqual(this.decoder.DecodeHand(hand), "Blue Two, Orange Three, Yellow Four, Green Two, Yellow Six");
+        }
+    }
+}
