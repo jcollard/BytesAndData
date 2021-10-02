3d260dc39e32a5879e6ecc0cef2dc044fbade21e
Joseph Collard <jcollard@users.noreply.github.com>
2021-10-02 11:39:33 -0700

    chore: Update settings to show tests in VS Code.

---
+++ tests/TestDecodeColor.cs
@@ -0,0 +1,28 @@
+namespace BytesAndData
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests <see cref="IFiveTwoOneOneDecoder"/> Decode Color method.
+    /// </summary>
+    public class TestDecodeColor
+    {
+        /// <summary>
+        /// Initializes the IFiveTwoOneOneDecoder by using <see
+        /// cref="Config.GetDecoder"/>.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Tests decoding a Yellow card encoding.
+        /// </summary>
+        [Test]
+        public void TestYellow()
+        {
+            Assert.Fail();
+        }
+    }
+}
